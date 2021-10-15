bb0c39da0f6f4e361e78bad754fa0cb785ce890c
Kuerl <taitran1401tn@gmail.com>
2021-10-15 17:30:42 +0700

    10/15/21 Kuerl: Create base controllers of team tasks and personal tasks

--- src/modules/tasks/controllers/task.controller.ts
+++ src/modules/tasks/controllers/task.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Put } from '@nestjs/common';
+import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
 
 @Controller('task')
 export class TaskController {
@@ -13,14 +13,44 @@ export class TaskController {
     return;
   }
 
-  @Put('personal/')
+  @Get('personal/:personalTaskId')
+  getDetailAPersonalTask() {
+    return;
+  }
+
+  @Put('personal/:personalTaskId')
   editAPersonalTask() {
     return;
   }
 
+  @Delete()
+  deleteAPersonalTask() {
+    return;
+  }
+
   // TEAM TASK
-  @Post('team/:team_Id')
+  @Post('team/:team_Id') // Assign teammates
   createATaskOfTeam() {
+    return;
+  }
+
+  @Get('team/user/:username')
+  getAllTeamTasks() {
+    return;
+  }
+
+  @Get('team/:teamTaskId')
+  getATeamTask() {
+    return;
+  }
+
+  @Put('team/:teamTaskId/:username')
+  editATeamTask() {
+    return;
+  }
+
+  @Delete('team/:teamTaskId/:username')
+  deleteATeamTask() {
     return;
   }
 }
