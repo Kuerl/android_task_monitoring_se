6f19cb5e627996b0f2a7977be92b1560816e4b93
Kuerl <taitran1401tn@gmail.com>
2021-10-21 11:20:52 +0700

    10/20/21 Kuerl: Message services

--- src/modules/messages/message.module.ts
+++ src/modules/messages/message.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { MessageEntity } from './entities/message.entity';
+import { MessageController } from './controllers/message.controller';
+import { MessageService } from './services/message.service';
+import { MessageRepository } from './repositories/message.repository';
+import { TeamRepository } from '../teams/repositories/team.repositories';
+import { UserRepository } from '../users/repositories/user.repository';
+import { TeamUserRepository } from '../teams/repositories/team-user.repository';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([MessageEntity])],
-  controllers: [],
-  providers: [],
+  imports: [
+    TypeOrmModule.forFeature([
+      MessageRepository,
+      TeamRepository,
+      UserRepository,
+      TeamUserRepository,
+    ]),
+  ],
+  controllers: [MessageController],
+  providers: [MessageService],
 })
 export class MessageModule {}
